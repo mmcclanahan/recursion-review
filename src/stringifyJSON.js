5da2b8ffdada67a7e7f1905cfab01b128482d738
mmcclanahan <mmmcclan365@gmail.com>
2024-01-22 22:33:30 -0800

    completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,21 +8,15 @@ var stringifyJSON = function(obj) {
   //string base case
   if (typeof obj === 'string') {
     return '\"' + obj + '\"';
-  }
-  //number,boolean base case
-  else if (typeof obj === 'number' || typeof obj === 'boolean') {
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
     return obj.toString();
-  }
-  //null
-  else if (typeof obj === 'null') {
+  } else if (typeof obj === 'null') {
     return 'null';
-  }
-  //undefined
-  else if (!obj) {
+  } else if (typeof obj === 'function' || obj === undefined) {
+    return undefined;
+  } else if (!obj) {
     return 'null';
-  }
-  //array
-  if (Array.isArray(obj)) {
+  } else if (Array.isArray(obj)) {
     resultStr += '[';
     if (obj.length > 0 && obj !== undefined) {
       for (var i = 0; i < obj.length - 1; i++) {
@@ -32,35 +26,25 @@ var stringifyJSON = function(obj) {
       resultStr += stringifyJSON(obj[obj.length - 1]);
     }
     resultStr += ']';
-    // comma
     return resultStr;
-  //'[null]' to equal '[]'
   } else if (typeof obj === 'object') {
-    resultStr += '{';
-    //function basecase
-    // Mabe try putting edge cases of functions or undefined for values here?
-    if (Object.keys(obj.length) === 0) {
-      resultStr += '}';
+    if (Object.keys(obj).length === 0) {
+      return '{}';
     } else {
+      resultStr += '{';
       for (var key in obj) {
-        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
-          resultStr += '';
-        } else {
-          resultStr += stringifyJSON(key) + ': ' + stringifyJSON(obj[key]) + ',';
+        var keyStr = stringifyJSON(key);
+        var valueStr = stringifyJSON(obj[key]);
+        if (valueStr !== undefined && valueStr !== undefined) {
+          resultStr += keyStr + ':' + valueStr + ',';
         }
       }
-      resultStr = resultStr.slice(0, resultStr.length - 1) + '}';
+      var commaIndex = resultStr.lastIndexOf(',');
+      resultStr = resultStr.slice(0, commaIndex) + resultStr.slice(commaIndex + 1) + '}';
     }
     return resultStr;
   }
 
 };
-/*
-Cannot convert undefined or null to object
-at Function.keys (<anonymous>)
-    at stringifyJSON (src/stringifyJSON.js:40:16)
-    at file:///Users/toast/Desktop/HR%20sprints/rfp2401-recursion-review/spec/stringifyJSONSpec.js:7:20
-    at Array.forEach (<anonymous>)
-    at Context.<anonymous> (spec/stringifyJSONSpec.js:5:26)
-*/
-// obj.forEach(function(element)
+
+
